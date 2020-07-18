0eff2afc3d2900be1b6c9bf71e611e77e665c1fa
Adam Bliss <abliss@gmail.com>
2020-07-17 20:32:44 -0400

    Fix typo in curl cmdline flag

--- .sandstorm/setup.sh
+++ .sandstorm/setup.sh
@@ -12,6 +12,6 @@ rm -rf /etc/nginx/sites-enabled/default
 # TODO: debian-stretch has only capnproto v 0.5 but we need 0.6 or better
 pushd /tmp
 curl --silent https://ftp.gnome.org/ubuntu/ubuntu/pool/main/c/capnproto/libcapnp-dev_0.7.0-5_amd64.deb -O
-curl --slient http://ftp.ubuntu.com/ubuntu/ubuntu/pool/universe/c/capnproto/capnproto_0.7.0-5_amd64.deb -O
+curl --silent http://ftp.ubuntu.com/ubuntu/ubuntu/pool/universe/c/capnproto/capnproto_0.7.0-5_amd64.deb -O
 sudo dpkg -i libcapnp-dev_0.7.0-5_amd64.deb capnproto_0.7.0-5_amd64.deb
 popd
